f50b7c6d47e29b35573fd9dffc088c72dc966d42
OlegKolyushko <kolyushko.oleg@gmail.com>
2020-09-05 14:22:59 +0300

    new7

---
+++ HomeWork7/src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import './task/task1';
+import './task/task2';
+import './task/task3';
+import './task/task4';
+import './task/task5';
+import './task/task6';
+import './task/task7';
